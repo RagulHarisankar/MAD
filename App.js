9e78826cf7fea24e60976392bb040863974785f3
RagulHarisankar <40146168+RagulHarisankar@users.noreply.github.com>
2023-08-05 12:50:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { View, Text, StyleSheet, Image } from 'react-native'; 
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Input, Icon } from '@rneui/themed';
 
 export default function App(){
   return(
     <View style={parent.box}>
       <Image source={require('./assets/login.png')} style={parent.img}/>
       <View style={parent.textBox}>
-        <Text style={{ fontWeight: 'bold', fontSize: 24 }}>Login</Text>
+        <Text style={{ fontWeight: 'bold', fontSize: 30, marginLeft: 10 }}>Login</Text>
+        <Input
+          placeholder='Email'
+          leftIcon={{ type: 'material', name: 'email' }}
+        />
+        <Input
+          placeholder='Password'
+          leftIcon={{ type: 'material', name: 'lock' }}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={parent.btn}>
+          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Login</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -16,13 +29,22 @@ let parent = StyleSheet.create({
     width: '100%',
     height: '100%',
     alignItems: 'center',
-    paddingHorizontal: 16
+    paddingHorizontal: 20,
+    justifyContent: 'center'
   },
   img: {
-    width: '70%',
+    width: '100%',
     height: '30%'
   },
   textBox: {
     width: '100%',
+    height: '35%',
+    justifyContent: 'space-around'
+  },
+  btn: {
+    backgroundColor: '#0065ff',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 10
   }
 })
